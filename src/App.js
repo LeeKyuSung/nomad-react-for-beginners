bcc1e1604f85749d1ce5460aa312eff4a48aa6d5
LeeKyuSung <ks5050577@gmail.com>
2023-04-09 01:13:59 +0900

    6.2 Deps

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import Button from "./Button";
 import styles from "./App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, []);
+  useEffect(() => {
+    if (keyword.length > 5) console.log("SEARCH FOR", keyword);
+  }, [keyword]);
+  useEffect(() => {
+    console.log("counter changed", counter);
+  }, [counter]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here "
+      />
       <h1 className={styles.title}>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
