2274eb3e9910cedb9addfeed265111749ef5e8dc
LeeKyuSung <ks5050577@gmail.com>
2023-04-09 12:04:09 +0900

    6.4 Cleanup

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
+function Hello() {
+  useEffect(() => {
+    console.log("Hello");
+    return () => console.log("Bye");
+  }, []);
+  return <div>Hello</div>;
+}
 function App() {
   const [counter, setValue] = useState(0);
   const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
+  const [showing, setShowing] = useState(false);
+  const onClick2 = () => setShowing((prev) => !prev);
   console.log("i run all the time");
   useEffect(() => {
     console.log("CALL THE API...");
@@ -27,6 +36,8 @@ function App() {
       />
       <h1 className={styles.title}>{counter}</h1>
       <button onClick={onClick}>click me</button>
+      <button onClick={onClick2}>{showing ? "Hide" : "Show"}</button>
+      {showing ? <Hello /> : null}
     </div>
   );
 }
